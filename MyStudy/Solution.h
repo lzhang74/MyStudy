625a70d8dd84a94feafa76b573218e3743bb3683
lzhang74 <66996143+lzhang74@users.noreply.github.com>
2022-04-24 20:16:26 +0800

    add more answers

--- MyStudy/Solution.h
+++ MyStudy/Solution.h
@@ -6,6 +6,7 @@
 #define UNTITLED4_SOLUTION_H
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 extern int findContentChildren(vector<int>& children, vector<int>& cookie);
 class Solution {
@@ -18,6 +19,174 @@ public:
                 }
             }
         }
+        return {};
+    }
+    int eraseOverlapIntervals_435(vector<vector<int>>& intervals){
+        if(intervals.empty()){
+            return 0;
+        }
+        int size = intervals.size();
+        sort(intervals.begin(),intervals.end(), [](const auto& u, const auto& v){
+            return u[1] < v[1];
+        });
+        int right = intervals[0][1];
+        int num = 1;
+        for(int i=1;i<size;i++){
+            if(intervals[i-1][1] <= intervals[i][0]){
+                num++;
+                right = intervals[i][1];
+            }
+        }
+        for(int i=0;i<size;i++)
+            cout << intervals[i][0] << " " << intervals[i][1] << endl;
+        return num;
+    }
+    int eraseOverlapIntervals_435_2(vector<vector<int>>& intervals){
+        if(intervals.empty()){
+            return 0;
+        }
+        int size = intervals.size();
+        sort(intervals.begin(),intervals.end(), [](const auto& u, const auto& v){
+            return u[0] < v[0];
+        });
+        vector<int> f(size,1);
+        for(int i=1;i<size;i++){
+            for(int j=0;j<i;j++){
+               if(intervals[i][0] >= intervals[j][1]){
+                  f[i] = max(f[j]+1, f[i]);
+               }
+            }
+        }
+
+        for(int i=0;i<size;i++)
+            cout << intervals[i][0] << " " << intervals[i][1] << endl;
+        return size - *max_element(f.begin(),f.end());
+
+    }
+    int findContentChildren(vector<int>& children, vector<int>& cookie){
+        int children_index = 0, cookie_index = 0;
+        sort(children.begin(),children.end());
+        sort(cookie.begin(),cookie.end());
+        while(children_index < children.size() && cookie_index < cookie.size())         {
+            if(children[children_index] <= cookie[cookie_index]){
+                children_index++;
+            }
+            cookie_index++;
+        }
+
+        return children_index;
+    }
+    int candy_135(vector<int>& ratings){
+        int size = ratings.size();
+        if(size < 2){
+            return size;
+        }
+        vector<int> num(size,1);
+        for(int i=1;i<size;i++){
+            if(ratings[i] > ratings[i-1]){
+                num[i] = num[i-1] +1;
+            }
+        }
+        for(int i=0; i<size; i++){
+            cout << num[i] << endl;
+        }
+        for(int i=size-1;i>=1;i--){
+            if(ratings[i] < ratings[i-1]){
+                num[i-1] = max(num[i-1],num[i]+1);
+            }
+        }
+        cout << endl;
+        for(int i=0; i<size; i++){
+            cout << num[i] << endl;
+        }
+        return accumulate(num.begin(),num.end(),0);
+    }
+    int findMinArrowShots_452(vector<vector<int>>& points){
+        int size = points.size();
+        if(size < 2){
+            return 1;
+        }
+        sort(points.begin(),points.end(),[](const auto& u, const auto& v){
+            return u[1] < v[1];
+        });
+        int out=1, j=1, right = points[0][1];
+        while(j < size){
+            if(right < points[j][0]){
+               out++;
+               right = points[j][1];
+            }
+//            if(right >= points[j][0]){
+//                if(right > points[j][1]){
+//                    right = points[j][1];
+//                }
+//            }else{
+//                out++;
+//                right = points[j][1];
+//            }
+            j++;
+        }
+        return out;
+    }
+    vector<int> partitionLabels_763(string s){
+        int size = s.size();
+        vector<vector<int>> table{26,{1000,1000}};
+        for(int i=0;i<size;i++){
+            if(table[s[i] - 'a'][0] == 1000){
+               table[s[i] - 'a'][0] = i;
+               table[s[i] - 'a'][1] = i;
+            }else{
+                table[s[i] - 'a'][1] = i;
+            }
+        }
+        sort(table.begin(),table.end(),[](const auto& u, const auto& v){
+            return u[0] < v[0];
+        });
+        vector<int> partition;
+        int start=0,end=table[0][1];
+        int last_end = 1000;
+        for(int i=1;i<26;i++){
+           if(table[i][0] < end){
+               if(table[i][1] > end) {
+                   end = table[i][1];
+               }
+           }else{
+               partition.push_back(end-start+1);
+               last_end = end;
+               if(table[i][0] == 1000){
+                   break;
+               }else{
+                   start = table[i][0];
+                   end = table[i][1];
+
+               }
+           }
+        }
+        if(end != last_end){
+            partition.push_back(end-start+1);
+        }
+        return partition;
+    }
+    vector<int> partitionLables_763_2(string s){
+        int size = s.size();
+        int table[26];
+        vector<int> partition;
+        for(int i=0;i<size;i++){
+           table[s[i]-'a'] = i;
+        }
+        int start = 0, end = 0;
+        for(int i=0;i<size;i++){
+            if(table[s[i]-'a'] > end){
+                end = table[s[i]-'a'];
+            }
+            if(i==end){
+                partition.push_back(end-start+1);
+                if((i+1)<size){
+                    start = i+1;
+                    end = table[s[i+1]-'a'];
+                }
+            }
+        }
+        return partition;
     }
 };
 struct ListNode {
--- MyStudy/main.cpp
+++ MyStudy/main.cpp
@@ -5,9 +5,26 @@
 #include "test.h"
 using namespace std;
 
+#define LEETCODE_NUM_EN   763//435
+
 int main() {
-    vector<int> child{4,21,13,11,3};
-    vector<int> cookie{5,5,5,5,3};
-    cout << findContentChildren(child,cookie);
+    Solution S;
+#if LEETCODE_NUM_EN == 435
+    vector<vector<int>> intervals{{2,3},{1,2},{3,4},{2,8}};
+    cout << S.eraseOverlapIntervals_435_2(intervals) << endl;
+#elif LEETCODE_NUM_EN == 135
+    vector<int> candy{2,1,0,3,4,3,3,2,1};
+    cout << S.candy_135(candy);
+#elif LEETCODE_NUM_EN == 452
+//    vector<vector<int>> points{{10,16},{2,8},{1,6},{7,12}};
+    vector<vector<int>> points{{1,2},{3,4},{5,6},{7,8}};
+    cout << S.findMinArrowShots_452(points);
+#elif LEETCODE_NUM_EN == 763
+    string str = "ababcbacadefegdehijhklij";
+    vector<int> partition;
+    partition = S.partitionLables_763_2(str);
+    for(int i=0;i<partition.size();i++)
+        cout << partition[i] << endl;
+#endif
     return 0;
 }
